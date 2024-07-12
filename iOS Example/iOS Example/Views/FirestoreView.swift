efb792eddc541a3dfac3fb9b626d6c6a5487e32c
Michal Žampach <zampach.michal.dev@gmail.com>
2024-07-12 06:27:03 +0200

    fix: example build

--- iOS Example/iOS Example/Views/FirestoreView.swift
+++ iOS Example/iOS Example/Views/FirestoreView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import Firebase
 import FirebaseAuth
 import FirebaseFirestore
-import FirebaseFirestoreSwift
 
 struct FirestoreView: View {
     @StateObject private var viewModel = FirestoreViewModel()
